f19b82bbdd0b85d5e3cca2985ae8dc66d9482ce5
Gabriel Valle <gvalle91@gmail.com>
2018-06-16 21:03:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,8 @@ class Song
   end
   
   def find_or_create_by_name(name)
-    
+    self.find_by_name(name) if self.find_by_name(name)
+    self.create_by_name if !self.find_by_name(name)
   end
   
 end
