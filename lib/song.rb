f8e97e55754c9926e314ed8bf461f04a5f1063df
Gabriel Valle <gvalle91@gmail.com>
2018-06-16 20:29:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,10 @@ class Song
     new_song.save
   end
   
-  def self.new_by_name
-    
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song.save
   end
   
 end
